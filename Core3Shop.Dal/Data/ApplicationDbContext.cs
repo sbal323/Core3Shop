9a543e494f9c6b4d7b41fc1598ae432f43b80274
sbalog <sbalog@lanteria.net>
2019-08-16 17:23:30 +0300

    Project reorganized

--- Core3Shop.Dal/Data/ApplicationDbContext.cs
+++ Core3Shop.Dal/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Core3Shop.Data
+namespace Core3Shop.Dal.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
